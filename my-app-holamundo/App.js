334a3b126d53d45f9ee46dca690aca3d005bca12
Ramonrx86 <joseramon.perez@alumnos.ulagos.cl>
2024-11-03 23:29:22 -0300

    correcciones

--- my-app-holamundo/App.js
+++ my-app-holamundo/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>¡Hola Mundo!</Text>
+      <Text style={styles.text}>¡Hola xx Mundo!</Text>
     </View>
   );
 }
